d37031d947319af63648d68e06e895129d21f8d5
thundax-lyp <731378491@qq.com>
2026-03-12 10:00:05 +0800

    refactor

--- src/api/admin/system.ts
+++
@@ -1,9 +0,0 @@
-import request from '/@/utils/request';
-
-// 系统缓存监控
-export function systemCache() {
-	return request({
-		url: '/admin/system/cache',
-		method: 'get',
-	});
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig, loadEnv, ConfigEnv } from 'vite';
-import VueSetupExtend from 'unplugin-vue-setup-extend/vite';
-import AutoImport from 'unplugin-auto-import/vite';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import viteCompression from 'vite-plugin-compression';
-// @ts-ignore
-import { svgBuilder } from '/@/components/IconSelector/index';
 
 const pathResolve = (dir: string) => {
 	return resolve(__dirname, '.', dir);
 };
 
 const alias: Record<string, string> = {
-	'/@': pathResolve('./src/'),
-	'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+	'@': pathResolve('./src/'),
 };
 
 const viteConfig = defineConfig((mode: ConfigEnv) => {
@@ -23,13 +18,7 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 	const isDev = env.ENV === 'development'
 	return {
 		plugins: [
-			vue(), // Vue 插件
-			svgBuilder('./src/assets/icons/'), // 将 SVG 文件转换成 Vue 组件
-			VueSetupExtend({}), // setup语法糖增强插件
-			AutoImport({
-				imports: ['vue', 'vue-router', 'pinia'], // 自动导入的依赖库数组
-				dts: './auto-imports.d.ts', // 自动导入类型定义文件路径
-			}),
+			react(),
 			topLevelAwait({
 				promiseExportName: '__tla', // TLA Promise 变量名
 				promiseImportName: (i) => `__tla_${i}`, // TLA Promise 导入名
@@ -41,9 +30,6 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 		root: process.cwd(), // 项目根目录
 		resolve: { alias }, // 路径别名配置
 		base: mode.command === 'serve' ? './' : env.VITE_PUBLIC_PATH,
-		optimizeDeps: {
-			include: ['element-plus/es/locale/lang/zh-cn', 'element-plus/es/locale/lang/en'],
-		},
 		server: {
 			host: '0.0.0.0', // 服务器地址
 			port: env.VITE_PORT as unknown as number, // 服务器端口号
@@ -84,17 +70,13 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 					assetFileNames: `assets/[name].[hash].[ext]`,
 					compact: true,
 					manualChunks: {
-						vue: ['vue', 'vue-router', 'pinia'],
-						echarts: ['echarts'],
+						react: ['react', 'react-dom', 'react-router-dom'],
 					},
 				},
 			},
 		},
 		css: { preprocessorOptions: { css: { charset: false } } },
 		define: {
-			__VUE_I18N_LEGACY_API__: JSON.stringify(false),
-			__VUE_I18N_FULL_INSTALL__: JSON.stringify(false),
-			__INTLIFY_PROD_DEVTOOLS__: JSON.stringify(false),
 			__VERSION__: JSON.stringify(process.env.npm_package_version),
 			__NEXT_NAME__: JSON.stringify(process.env.npm_package_name),
 		},
